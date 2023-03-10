7708e1cf968275db0d3d58638b46f9418258e2a1
Leozbroca <leo.brokka@gmail.com>
2023-03-10 17:17:26 -0300

    Feat: Carrossel(imcompleto)

--- encantada-saboaria/src/components/Carousel/Styles.tsx
+++ encantada-saboaria/src/components/Carousel/Styles.tsx
@@ -1,6 +1,20 @@
 import styled from "styled-components";
 
 export const ImgCar = styled.img`
-width:500px;
-height:300px;
+width:252px;
+height:252px;
+`
+
+export const Main = styled.div`
+width:75%;
+margin-top:50px;
+z-index:1;
+
+.alice-carousel__next-btn{
+    width:500px;
+}
+
+.alice-carousel__stage-item{
+    
+}
 `
